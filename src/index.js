c7a320154d3b2d26114ed039a7e5da9b068ff725
Tomas Bulva <megarameno@gmail.com>
2018-01-17 23:04:51 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,68 @@
 'use strict';
 
 import { EventEmitter } from 'events';
+import loudness from 'loudness';
 
-export default class Text extends EventEmitter {
-    constructor(options, output) {
+export default class Volume extends EventEmitter {
+    
+    constructor(
+        options, 
+        output
+    ) {
         super();
         options = options || {};
         this.output = output || {};
-
-        //custom config
-        this.text = options.text || '';
-        this.secretValue = options.secretValue;
-
     }
 
     update() {
-        //update output
-        this.output.full_text = this.text;
-        this.output.short_text = this.text;
 
-        //emit updated event to i3Status
-        this.emit('updated', this, this.output);
+        var volumeMute = '<span font="Material-Design-Iconic-Font" size="large"></span>';
+        var volumeLow = '<span font="Material-Design-Iconic-Font" size="large"></span>';
+        var volumeMid = '<span font="Material-Design-Iconic-Font" size="large"></span>';
+        var volumeHi = '<span font="Material-Design-Iconic-Font" size="large"></span>';
+        var headphones = '<span font="Material-Design-Iconic-Font" size="large"></span>'; 
+
+        this.emit('pause', this);
+
+        loudness.getMuted((err, mute) => {
+            // mute = false
+
+            if ( mute ) {
+                
+                this.output.full_text = volumeMute;
+                this.output.short_text = volumeMute;
+
+
+                this.emit('resume', this);
+                this.emit('updated', this, this.output);
+
+                return false;
+            }
+
+            loudness.getVolume((err, vol) => {
+                // Done
+                var output = "";
+
+                switch(true) {
+                    case (vol >= 80):
+                        output = volumeHi;
+                    break;
+                    case (vol < 80 && vol > 20):
+                        output = volumeMid;
+                    break;
+                    case (vol <= 20):
+                        output = volumeLow;
+                    break;
+                }
+
+                this.output.full_text = output;
+                this.output.short_text = output;
+
+                this.emit('resume', this);
+                this.emit('updated', this, this.output);
+
+            });
+        });
     }
 
     action(action) {
